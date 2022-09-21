a6281b9e70b623010f9a5d741ed7d149e93c85d9
ibtihel <mejriibtihel90@gmail.com>
2022-09-21 15:47:07 +0100

    consume endpoint register and display alert

--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -10,25 +10,31 @@ import { AuthService } from "../auth/auth.service";
 export class SignupComponent implements OnInit {
   @ViewChild("signupForm") signupForm: NgForm | undefined;
   constructor(private authService: AuthService) {}
+  isSuccess = false;
+  isFailed = false;
 
   ngOnInit(): void {}
 
-  // onSubmit(form: NgForm) {
-  //   console.log(form.value);
-  //   const email = form.value.email;
-  //   const password = form.value.password;
-  //   // this.authService.signup(email,password).subscribe(resData => {
-  //   //   console.log('resData',resData);
-  //   // },
-  //   // error => {
-  //   //   console.log('error', error);
-  //   //}
-  //   //)
-  //   // form.reset();
-  // }
-
-  onSubmit(){
-    console.log(this.signupForm);
-    
+  onSubmit() {
+    console.log(this.signupForm?.form);
+    const email = this.signupForm?.form.value.email;
+    const password = this.signupForm?.form.value.password;
+    this.authService.signup(email, password).subscribe(
+      (resData) => {
+        console.log("resData", resData);
+        this.isSuccess = true;
+        setTimeout(() => {
+          this.isSuccess = false;
+        }, 4000);
+      },
+      (error) => {
+        console.log("error", error);
+        this.isFailed = true;
+        setTimeout(() => {
+          this.isFailed = false;
+        }, 6000);
+      }
+    );
+    this.signupForm?.reset();
   }
 }
