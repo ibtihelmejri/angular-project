9107953da8cf55eface86c304375d4fc513541ea
ibtihel <mejriibtihel90@gmail.com>
2022-10-01 09:40:11 +0100

    add data signup to store

--- src/app/auth/store/auth.reducer.ts
+++ src/app/auth/store/auth.reducer.ts
@@ -1,8 +1,7 @@
 import { AuthToken } from "src/app/communComponents/auth.model";
 import * as AuthActions from "./auth.actions";
 
-
-export interface authState{
+export interface authState {
   authToken: AuthToken | null;
   authError: string | null;
   loading: boolean;
@@ -11,38 +10,36 @@ export interface authState{
 const initialState: authState = {
   authToken: null,
   authError: null,
-  loading:false
+  loading: false,
 };
 
+export function authReducer(
+  state: authState = initialState,
+  action: AuthActions.AuthActions
+): authState {
+  switch (action.type) {
+    case AuthActions.LOGIN:
+      return {
+        ...state,
+        authError: null,
+        authToken: action.payload,
+        loading: false,
+      };
 
-
-export function authReducer (state : authState = initialState, action: AuthActions.AuthActions): authState {
-    switch (action.type) {
-      case AuthActions.LOGIN:
-        return {
-          ...state,
-          authError:null,
-          authToken: action.payload,
-          loading:false
-          };
-          
-        case AuthActions.LOGIN_START:
-          return {
-            ...state,
-            authError:null,
-            loading:true
-          };
-          case AuthActions.LOGIN_FAIL:
-            return {
-              ...state,
-              authToken:null,
-              authError:action.payload,
-              loading:false
-            }
-          default:
-            return state;
-        }
-
-
-    }
-   
+    case AuthActions.LOGIN_START:
+      return {
+        ...state,
+        authError: null,
+        loading: true,
+      };
+    case AuthActions.LOGIN_FAIL:
+      return {
+        ...state,
+        authToken: null,
+        authError: action.payload,
+        loading: false,
+      };
+    default:
+      return state;
+  }
+}
