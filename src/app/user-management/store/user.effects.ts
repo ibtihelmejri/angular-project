9b816cde9e72549f9ef85f03c036faba42533361
ibtihel <mejriibtihel90@gmail.com>
2022-10-04 16:17:06 +0100

    add new user

--- src/app/user-management/store/user.effects.ts
+++ src/app/user-management/store/user.effects.ts
@@ -3,38 +3,58 @@ import { Actions, createEffect, ofType } from "@ngrx/effects";
 import { catchError, map, switchMap, mergeMap } from "rxjs/operators";
 import * as UserActions from "./user.actions";
 import { Injectable } from "@angular/core";
-import { of, throwError } from "rxjs";
-import { UserResModel } from "src/app/communComponents/user_res.model";
-import {UserModel } from "../user.model"
-
+import { of } from "rxjs";
+import { UserModel } from "../user.model";
 
 export interface UserResponseData {
-     page: number;
-     per_page: number;
-     total: number;
-     total_page: number;
-     data: UserModel[];
-  }
-  
+  page: number;
+  per_page: number;
+  total: number;
+  total_page: number;
+  data: UserModel[];
+}
 
+export interface AddUserResponseData {
+  name: string;
+  job: string;
+  id: string;
+  createdAt: string;
+}
 
 @Injectable()
 export class UserEffects {
-userList = createEffect(() =>
-  
-  this.actions$.pipe(
-    ofType(UserActions.GET_USERS),
-    mergeMap(() => this.http.get<UserResponseData>('https://reqres.in/api/users?page=1')
-      .pipe(
-        map((data) => new UserActions.ActionSuccess(data)),
-        catchError((err) => of (new UserActions.ActionFail('error')))
-      ))
-    ));
+  userList = createEffect(() =>
+    this.actions$.pipe(
+      ofType(UserActions.GET_USERS),
+      mergeMap(() =>
+        this.http
+          .get<UserResponseData>("https://reqres.in/api/users?page=1")
+          .pipe(
+            map((data) => new UserActions.ActionSuccess(data)),
+            catchError((err) => of(new UserActions.ActionFail("error")))
+          )
+      )
+    )
+  );
 
+  addUser = createEffect(() =>
+    this.actions$.pipe(
+      ofType(UserActions.ADD_USER),
+      switchMap((userData: UserActions.AddUser) => {
+        return this.http
+          .post<AddUserResponseData>("https://reqres.in/api/users", {
+            name: userData.payload.name,
+            job: userData.payload.job,
+          })
+          .pipe(
+            map(
+              () => new UserActions.IsSuccess("L'ajout a été effectuée avec succès")
+              ),
+            catchError((err) => of(new UserActions.ActionFail("error")))
+          );
+      })
+    )
+  );
 
-  constructor(private actions$: Actions, private http: HttpClient) {
-    
-  }
+  constructor(private actions$: Actions, private http: HttpClient) {}
 }
-
-
